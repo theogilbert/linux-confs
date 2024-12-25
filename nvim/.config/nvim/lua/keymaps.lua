842e7bc37a64dbb3f0083625d0057d1bd493365c
theogilbert <theovgilbert@gmail.com>
2024-12-25 19:19:20 +0100

    configure :noh keymap

---
+++ nvim/.config/nvim/lua/keymaps.lua
@@ -0,0 +1,7 @@
+-- [[ Basic Keymaps ]]
+--  See `:help vim.keymap.set()`
+
+-- Clear highlights on search when pressing <Esc> in normal mode
+--  See `:help hlsearch`
+vim.keymap.set("n", "<Esc>", "<cmd>nohlsearch<CR>")
+

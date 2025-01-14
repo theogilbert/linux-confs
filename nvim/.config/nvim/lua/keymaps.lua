0b3d1e982293d295b8e166bd29ee7f01006e971a
theogilbert <theovgilbert@gmail.com>
2025-01-14 18:27:04 +0100

    add keymap to toggle gutter

--- nvim/.config/nvim/lua/keymaps.lua
+++ nvim/.config/nvim/lua/keymaps.lua
@@ -5,3 +5,13 @@
 --  See `:help hlsearch`
 vim.keymap.set("n", "<Esc>", "<cmd>nohlsearch<CR>")
 
+-- If the number column is displayed, hide it and hide the sign column
+-- Otherwise, display both.
+function toggleGutter()
+    isGutterDisplayed = vim.opt.number:get()
+
+    vim.opt.number = not isGutterDisplayed
+    vim.opt.signcolumn = isGutterDisplayed and "no" or "yes"
+end
+vim.keymap.set("n", "<Leader>n", toggleGutter)
+

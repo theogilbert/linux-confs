56911eef40c72da6e44afd7723df91298bf07d35
theogilbert <theovgilbert@gmail.com>
2025-02-21 17:31:26 +0100

    add shortcut to toggle mouse support

--- nvim/.config/nvim/lua/keymaps.lua
+++ nvim/.config/nvim/lua/keymaps.lua
@@ -13,5 +13,11 @@ function toggleGutter()
     vim.opt.number = not isGutterDisplayed
     vim.opt.signcolumn = isGutterDisplayed and "no" or "yes"
 end
-vim.keymap.set("n", "<Leader>n", toggleGutter)
+vim.keymap.set("n", "<Leader>on", toggleGutter, { desc = "Toggle [O]ption [N]umber" })
+
+function toggleMouse()
+    toggledSettings = vim.opt.mouse:get().a and "" or "a"
+    vim.opt.mouse = toggledSettings
+end
+vim.keymap.set("n", "<Leader>om", toggleMouse, { desc = "Toggle [O]ption [M]ouse" })
 

7c5d71b29b146431365b158911cc9d84eab78264
theogilbert <theovgilbert@gmail.com>
2025-03-03 19:01:06 +0100

    reduce intrusiveness of autocomplete

--- nvim/.config/nvim/lua/plugins.lua
+++ nvim/.config/nvim/lua/plugins.lua
@@ -70,6 +70,7 @@ wk.add({
 
 local cmp = require("cmp")
 cmp.setup({
+        preselect = cmp.PreselectMode.None,
 	snippet = {
 		-- REQUIRED - you must specify a snippet engine
 		expand = function(args)
@@ -89,7 +90,7 @@ cmp.setup({
 		["<C-f>"] = cmp.mapping.scroll_docs(4),
 		["<C-Space>"] = cmp.mapping.complete(),
 		["<C-e>"] = cmp.mapping.abort(),
-		["<CR>"] = cmp.mapping.confirm({ select = true }), -- Accept currently selected item. Set `select` to `false` to only confirm explicitly selected items.
+		["<CR>"] = cmp.mapping.confirm({ select = false }),
 	}),
 	sources = cmp.config.sources({
 		{ name = "nvim_lsp" },

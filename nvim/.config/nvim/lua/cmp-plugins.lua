a3680b9b532319e07a81e443ba91a84a0577d426
theogilbert <theovgilbert@gmail.com>
2025-05-12 07:40:12 +0200

    cleanup cmp config

--- nvim/.config/nvim/lua/cmp-plugins.lua
+++ nvim/.config/nvim/lua/cmp-plugins.lua
@@ -57,17 +57,12 @@ end
 -- setlocal completeopt=menuone,popup,noinsert
 cmp.setup({
 	enabled = function()
-		return vim.api.nvim_buf_get_option(0, "buftype") ~= "prompt" or require("cmp_dap").is_dap_buffer()
+                return vim.api.nvim_buf_get_option(0, "buftype") ~= "prompt" or require("cmp_dap").is_dap_buffer()
 	end,
 	preselect = cmp.PreselectMode.None,
 	snippet = {
-		-- REQUIRED - you must specify a snippet engine
 		expand = function(args)
-			-- vim.fn["vsnip#anonymous"](args.body) -- For `vsnip` users.
-			-- require('luasnip').lsp_expand(args.body) -- For `luasnip` users.
-			-- require('snippy').expand_snippet(args.body) -- For `snippy` users.
-			-- vim.fn["UltiSnips#Anon"](args.body) -- For `ultisnips` users.
-			vim.snippet.expand(args.body) -- For native neovim snippets (Neovim v0.10+)
+                    vim.snippet.expand(args.body) -- For native neovim snippets (Neovim v0.10+)
 		end,
 	},
 	window = {
@@ -91,7 +86,6 @@ cmp.setup({
 		autocomplete = false,
 	},
 	sorting = {
-		priority_weight = 1,
 		comparators = {
                     deprioritize_private,
                     custom_lsp_kind_comparator,

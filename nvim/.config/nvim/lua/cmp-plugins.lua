8ba885be61eca4fe533363b1fcfb99c7232bd805
theogilbert <theovgilbert@gmail.com>
2025-06-10 09:15:01 +0200

    add trailing slash on path auto-cmp

--- nvim/.config/nvim/lua/cmp-plugins.lua
+++ nvim/.config/nvim/lua/cmp-plugins.lua
@@ -130,7 +130,8 @@ cmp.setup.cmdline("/", {
 cmp.setup.cmdline(":", {
 	mapping = cmp.mapping.preset.cmdline(),
 	sources = cmp.config.sources({
-		{ name = "path" },
+            { name = "path", option = { trailing_slash = true } },
+            { name = "cmdline" },
 	}, {
 		{ name = "cmdline" },
 	}),

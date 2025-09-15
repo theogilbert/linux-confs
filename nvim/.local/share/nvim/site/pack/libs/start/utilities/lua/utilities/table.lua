fb7243a1de39e84c588e2cbd5ad5b0fb0a427605
theogilbert <theovgilbert@gmail.com>
2025-09-15 07:07:28 +0200

    refactor utilities.table

--- nvim/.local/share/nvim/site/pack/libs/start/utilities/lua/utilities/table.lua
+++ nvim/.local/share/nvim/site/pack/libs/start/utilities/lua/utilities/table.lua
@@ -7,9 +7,9 @@ local center = function(text, width)
     return string.rep(' ', before_len) .. text .. string.rep(' ', after_len)
 end
 
--- Builds a structure keeping track of the state of a single CSV cell
--- being parsed.
-local function build_cell_parser(start_idx, start_char)
+--- @return table data A structure keeping track of the state of a single CSV
+--- cell being parsed character by character.
+local function build_cell_data(start_idx, start_char)
     local end_pos = nil
     if start_char == ',' then
         end_pos = start_idx - 1
@@ -24,33 +24,33 @@ local function build_cell_parser(start_idx, start_char)
     }
 end
 
-local function parse_new_char(parser, idx, char)
+local function parse_new_char(cell_data, idx, char)
     if char == '"' then
-        parser.pending_quote = not parser.pending_quote
-    elseif char == ',' and (not parser.quoted or parser.pending_quote) then
-        parser.end_pos = idx - 1
-    end
-
-    if char ~= "," and char ~= '"' and parser.pending_quote then
+        cell_data.pending_quote = not cell_data.pending_quote
+    elseif char == ',' then
+        if not cell_data.quoted or cell_data.pending_quote then
+            cell_data.end_pos = idx - 1
+        end
+    elseif cell_data.pending_quote then
         return "Unexpected quote character at index " .. idx - 1
     end
 
     return nil
 end
 
-local function parse_end_of_line(parser, idx)
-    if parser.quoted and not parser.pending_quote then
-        return "Expected closing quote at index " .. idx .. " with cell starting at " .. parser.start_pos
+local function process_end_of_line(cell_data, idx)
+    if cell_data.quoted and not cell_data.pending_quote then
+        return "Expected quoted cell starting at " .. cell_data.start_pos .. " to be closed at " .. idx
     end
 
-    parser.end_pos = idx
+    cell_data.end_pos = idx
     return nil
 end
 
-local function extract_text_from_parser(parser, line)
-    local cell = line:sub(parser.start_pos, parser.end_pos)
+local function extract_text_from_cell_data(cell_data, line)
+    local cell = line:sub(cell_data.start_pos, cell_data.end_pos)
 
-    if parser.quoted then
+    if cell_data.quoted then
         local trimmed = cell:sub(2, #cell - 1)
         cell = trimmed:gsub('""', '"')
     end
@@ -59,41 +59,45 @@ local function extract_text_from_parser(parser, line)
 end
 
 
--- Return a table containing cells present in the line,
--- and a string or nil value indicating if an error was encountered
--- parsing the line
+--- Extract columns from a CSV line
+---
+--- @param csv_line string A single CSV line, using comma `,` as a separator.
+--- @return table columns The list of columns extracted from the CSV line.
+---   In case of a failure to parse the CSV line, the table will be empty.
+--- @return string|nil err An error message if the CSV line could not be parsed.
+---   Nil otherwise.
 local parse_csv_line = function(csv_line)
     local cells = {}
-    local current_cell_parser = nil
+    local current_cell_data = nil
 
     for idx = 1, #csv_line do
         local char = csv_line:sub(idx, idx)
 
-        if current_cell_parser == nil then
-            current_cell_parser = build_cell_parser(idx, char)
+        if current_cell_data == nil then
+            current_cell_data = build_cell_data(idx, char)
         else
-            local err = parse_new_char(current_cell_parser, idx, char)
+            local err = parse_new_char(current_cell_data, idx, char)
             if err ~= nil then
-                return nil, "Error parsing CSV line '" .. csv_line .. "': " .. err
+                return {}, "Error parsing CSV line '" .. csv_line .. "': " .. err
             end
         end
 
-        if current_cell_parser.end_pos ~= nil then
-            table.insert(cells, extract_text_from_parser(current_cell_parser, csv_line))
-            current_cell_parser = nil
+        if current_cell_data.end_pos ~= nil then
+            table.insert(cells, extract_text_from_cell_data(current_cell_data, csv_line))
+            current_cell_data = nil
         end
     end
 
-    if current_cell_parser ~= nil then
-        local err = parse_end_of_line(current_cell_parser, #csv_line)
+    if current_cell_data ~= nil then
+        local err = process_end_of_line(current_cell_data, #csv_line)
         if err ~= nil then
-            return nil, "Failed to parse line '" .. csv_line .. "': " .. err
+            return {}, "Failed to parse line '" .. csv_line .. "': " .. err
         end
 
-        if current_cell_parser.end_pos ~= nil then
-            table.insert(cells, extract_text_from_parser(current_cell_parser, csv_line))
+        if current_cell_data.end_pos ~= nil then
+            table.insert(cells, extract_text_from_cell_data(current_cell_data, csv_line))
         else
-            return nil, "Unexpected end of CSV line '" .. csv_line .. "'"
+            return {}, "Unexpected end of CSV line '" .. csv_line .. "'"
         end
     end
 
@@ -117,7 +121,8 @@ end
 ---
 ---@param csv_text string A textual CSV content, using comma (`,`) as separator.
 ---@return table FormattedTable A table with the following fields:
----  - `lines` (table): The input structure data
+---  - `lines` (table): The structured input data, in the form of a table whose elements
+---     represent a line. Each line is itself a table where each element is a cell.
 ---  - `columns_width` (table): The width of each column of the table.
 ---  - `text` (table): The formatted text of the table, in the form of a table of textual lines.
 ---@return string|nil err: Error message if parsing failed, or nil on success.
@@ -132,25 +137,24 @@ function M.from_csv(csv_text)
         table.insert(lines, cols)
     end
 
-    return M.from_lines(lines)
+    return M.from_structured_data(lines)
 end
 
 ---Creates a new formatted table object.
 ---
----@param lines table The structure data from which to create the formatted table.
+---@param lines table The structured data from which to create the formatted table.
 ---       This table should contain rows, where each row is itself a table containing multiple values.
 ---       All rows should contain the same number of elements.
 ---@return table FormattedTable A table with the following fields:
 ---  - `lines` (table): The input structure data
 ---  - `columns_width` (table): The width of each column of the table.
 ---  - `text` (table): The formatted text of the table, in the form of a table of textual lines.
-function M.from_lines(lines)
-    local cols_width = {}
-
+function M.from_structured_data(lines)
     -- First, we need to detect the width of each column
     -- so that all rows for a given column are written over
     -- the same number of characters, without truncating any
     -- data.
+    local cols_width = {}
     for _, cols in ipairs(lines) do
         update_cols_widths(cols, cols_width)
     end
@@ -175,4 +179,7 @@ function M.from_lines(lines)
     }
 end
 
+-- TODO get column under cursor
+
 return M
+

ae83d4961afaabfe0192ca9b68a079d5ea1df9fc
theogilbert <theovgilbert@gmail.com>
2025-11-01 22:38:08 +0100

    support Series expressions

--- nvim/.local/share/nvim/site/pack/debugger/start/nvim-dap-df/lua/nvim-dap-df-pane/evaluator.lua
+++ nvim/.local/share/nvim/site/pack/debugger/start/nvim-dap-df/lua/nvim-dap-df-pane/evaluator.lua
@@ -2,6 +2,11 @@ local dap = require("dap")
 
 local M = {}
 
+local Types = {
+    DataFrame = "DataFrame",
+    Series = "Series"
+}
+
 local function evaluate_expression(session, expr, callback)
 	local params = { expression = expr, context = "watch", frameId = session.current_frame.id }
 	session:request("evaluate", params, function(err, result)
@@ -18,10 +23,10 @@ local function check_expression_type(session, expr, callback)
 	evaluate_expression(session, expr, function(err, result)
 		if err ~= nil then
 			callback(err)
-		elseif result.type ~= "DataFrame" and result.type ~= "Series" then
-			callback("Expression is neither a DataFrame nor a Series")
+		elseif Types[result.type] == nil then
+			callback(result.type, "Expression is neither a DataFrame nor a Series, but a " .. result.type)
 		else
-			callback()
+			callback(result.type, nil)
 		end
 	end)
 end
@@ -31,12 +36,21 @@ local function evaluate_df_expression(session, df_expr, callback)
 	evaluate_expression(session, limited_expr, callback)
 end
 
-local function evaluate_df_dtypes(session, df_expr, callback)
+local function evaluate_df_dtypes(session, df_expr, type, callback)
+    local dtypes_expr = ""
+    if type == Types.DataFrame then
 	local idx_expr = df_expr .. ".index.dtype.name"
 	local cols_expr = "[" .. df_expr .. "[col].dtype.name for col in " .. df_expr .. ".columns]"
 
-	local dtypes_expr = "','.join([" .. idx_expr .. ", *" .. cols_expr .. "])"
-	evaluate_expression(session, dtypes_expr, callback)
+	dtypes_expr = "','.join([" .. idx_expr .. ", *" .. cols_expr .. "])"
+    else
+	local idx_expr = df_expr .. ".index.dtype.name"
+	local col_expr = df_expr .. ".dtype.name"
+
+	dtypes_expr = "','.join([" .. idx_expr .. ", " .. col_expr .. "])"
+    end
+
+    evaluate_expression(session, dtypes_expr, callback)
 end
 
 local function merge_data_and_dtypes(data_ret, dtypes_ret)
@@ -62,7 +76,8 @@ M.evaluate_expression = function(expression, on_result)
 		return
 	end
 
-	check_expression_type(session, expression, function(err)
+	check_expression_type(session, expression, function(type, err)
+                -- TODO : send type in callback, and use that type to evaluate df_types (currently fails in case of Series as no columns)
 		if err ~= nil then
 			on_result(err, nil)
 			return
@@ -74,7 +89,7 @@ M.evaluate_expression = function(expression, on_result)
 				return
 			end
 
-			evaluate_df_dtypes(session, expression, function(err, dtypes_ret)
+			evaluate_df_dtypes(session, expression, type, function(err, dtypes_ret)
 				if err ~= nil then
 					on_result(err, nil)
 					return

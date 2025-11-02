6069ec276c1190ed4f6b5ee5dd27968affc83a8a
theogilbert <theovgilbert@gmail.com>
2025-11-02 17:06:52 +0100

    reformat code

--- nvim/.local/share/nvim/site/pack/debugger/start/nvim-dap-df/lua/nvim-dap-df-pane/evaluator.lua
+++ nvim/.local/share/nvim/site/pack/debugger/start/nvim-dap-df/lua/nvim-dap-df-pane/evaluator.lua
@@ -4,8 +4,8 @@ local M = {}
 
 --- @enum Types
 local Types = {
-    DataFrame = "DataFrame",
-    Series = "Series"
+	DataFrame = "DataFrame",
+	Series = "Series",
 }
 ---
 --- @class EvaluationState
@@ -19,11 +19,11 @@ local Types = {
 --- @field dtypes table|nil The CSV line representing the dtypes of the columns
 
 local function init_evaluation_state(callback, type)
-    if Types[type] == nil then
-            return nil, "Expression is neither a DataFrame nor a Series, but a " .. type
-    end
+	if Types[type] == nil then
+		return nil, "Expression is neither a DataFrame nor a Series, but a " .. type
+	end
 
-    return { callback = callback, type = type, sent = false }, nil
+	return { callback = callback, type = type, sent = false }, nil
 end
 
 local function merge_data_and_dtypes(data_ret, dtypes_ret)
@@ -32,38 +32,38 @@ local function merge_data_and_dtypes(data_ret, dtypes_ret)
 end
 
 local function send_if_state_ready(state)
-    local attrs = {'row_len', 'col_len', 'data', 'dtypes'}
-    for _, field in ipairs(attrs) do
-        if state[field] == nil then
-            return
-        end
-    end
+	local attrs = { "row_len", "col_len", "data", "dtypes" }
+	for _, field in ipairs(attrs) do
+		if state[field] == nil then
+			return
+		end
+	end
 
-    state.sent = true
+	state.sent = true
 
-    local merged = merge_data_and_dtypes(state.data, state.dtypes)
-    local shape = { state.col_len, state.row_len }
+	local merged = merge_data_and_dtypes(state.data, state.dtypes)
+	local shape = { state.col_len, state.row_len }
 
-    state.callback(merged, shape, nil)
+	state.callback(merged, shape, nil)
 end
 
 local function fail_evaluation_state(state, err)
-    if state.sent then
-        return
-    end
+	if state.sent then
+		return
+	end
 
-    state.sent = true
-    state.callback(nil, nil, err)
+	state.sent = true
+	state.callback(nil, nil, err)
 end
 
 local function update_evaluation_state(state, field, value)
-    if state.sent then
-        return
-    end
+	if state.sent then
+		return
+	end
 
-    state[field] = value
+	state[field] = value
 
-    send_if_state_ready(state)
+	send_if_state_ready(state)
 end
 
 local function evaluate_expression(session, expr, callback)
@@ -86,57 +86,55 @@ end
 ---                          False otherwise.
 --- @param session any The DAP session object
 local function evaluate_state_field(state, field, expr, str_value, session)
-    evaluate_expression(session, expr, function(err, result)
-        if err ~= nil then
-            fail_evaluation_state(state, err)
-        else
-            local value = result.lines
-            if str_value then
-                value = value:gsub("\\n", "\n"):sub(2, -2)
-            end
+	evaluate_expression(session, expr, function(err, result)
+		if err ~= nil then
+			fail_evaluation_state(state, err)
+		else
+			local value = result.lines
+			if str_value then
+				value = value:gsub("\\n", "\n"):sub(2, -2)
+			end
 
-            update_evaluation_state(state, field, value)
-        end
-
-    end)
+			update_evaluation_state(state, field, value)
+		end
+	end)
 end
 
 local function evaluate_df_data(state, df_expr, limit, session)
-    local limited_expr = df_expr .. ".head(" .. limit .. ").to_csv()"
-    evaluate_state_field(state, "data", limited_expr, true, session)
+	local limited_expr = df_expr .. ".head(" .. limit .. ").to_csv()"
+	evaluate_state_field(state, "data", limited_expr, true, session)
 end
 
 local function evaluate_df_dtypes(state, df_expr, session)
-    local dtypes_expr = ""
-    if state.type == Types.DataFrame then
-	local idx_expr = df_expr .. ".index.dtype.name"
-	local cols_expr = "[" .. df_expr .. "[col].dtype.name for col in " .. df_expr .. ".columns]"
+	local dtypes_expr = ""
+	if state.type == Types.DataFrame then
+		local idx_expr = df_expr .. ".index.dtype.name"
+		local cols_expr = "[" .. df_expr .. "[col].dtype.name for col in " .. df_expr .. ".columns]"
 
-	dtypes_expr = "','.join([" .. idx_expr .. ", *" .. cols_expr .. "])"
-    else
-	local idx_expr = df_expr .. ".index.dtype.name"
-	local col_expr = df_expr .. ".dtype.name"
+		dtypes_expr = "','.join([" .. idx_expr .. ", *" .. cols_expr .. "])"
+	else
+		local idx_expr = df_expr .. ".index.dtype.name"
+		local col_expr = df_expr .. ".dtype.name"
 
-	dtypes_expr = "','.join([" .. idx_expr .. ", " .. col_expr .. "])"
-    end
+		dtypes_expr = "','.join([" .. idx_expr .. ", " .. col_expr .. "])"
+	end
 
-    evaluate_state_field(state, "dtypes", dtypes_expr, true, session)
+	evaluate_state_field(state, "dtypes", dtypes_expr, true, session)
 end
 
 local function evaluate_col_count(state, df_expr, session)
-    if state.type == Types.Series then
-        update_evaluation_state(state, "col_len", "1")
-    else
-        local cols_count_expr = "len(" .. df_expr .. ".columns)"
-        evaluate_state_field(state, "col_len", cols_count_expr, false, session)
-    end
+	if state.type == Types.Series then
+		update_evaluation_state(state, "col_len", "1")
+	else
+		local cols_count_expr = "len(" .. df_expr .. ".columns)"
+		evaluate_state_field(state, "col_len", cols_count_expr, false, session)
+	end
 end
 
 local function evaluate_row_count(state, df_expr, session)
-    local row_count_expr = "len(" .. df_expr .. ")"
-    evaluate_state_field(state, "row_len", row_count_expr, false, session)
+	local row_count_expr = "len(" .. df_expr .. ")"
+	evaluate_state_field(state, "row_len", row_count_expr, false, session)
 end
-
 
 --- Parses TSNode objects matching queries present in queries/<filetype>/sections.scm
 --- @param expression string A python expression resolving to a pd.DataFrame or pd.Series object
@@ -157,23 +155,23 @@ M.evaluate_expression = function(expression, limit, on_result)
 		return
 	end
 
-        evaluate_expression(session, expression, function(err, result)
-            if err ~= nil then
-                on_result(nil, nil, err)
-                return
-            end
+	evaluate_expression(session, expression, function(err, result)
+		if err ~= nil then
+			on_result(nil, nil, err)
+			return
+		end
 
-            local state, err = init_evaluation_state(on_result, result.type)
+		local state, err = init_evaluation_state(on_result, result.type)
 
-            if err ~= nil then
-                on_result(nil, nil, err)
-            else
-                evaluate_df_data(state, expression, limit, session)
-                evaluate_df_dtypes(state, expression, session)
-                evaluate_row_count(state, expression, session)
-                evaluate_col_count(state, expression, session)
-            end
-        end)
+		if err ~= nil then
+			on_result(nil, nil, err)
+		else
+			evaluate_df_data(state, expression, limit, session)
+			evaluate_df_dtypes(state, expression, session)
+			evaluate_row_count(state, expression, session)
+			evaluate_col_count(state, expression, session)
+		end
+	end)
 end
 
 return M

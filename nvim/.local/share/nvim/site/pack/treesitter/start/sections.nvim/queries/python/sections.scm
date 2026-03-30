1a8919c5e2d484a161d21c801202fddde1047436
theogilbert <theovgilbert@gmail.com>
2026-03-30 07:33:29 +0200

    fix python sections query file

--- nvim/.local/share/nvim/site/pack/treesitter/start/sections.nvim/queries/python/sections.scm
+++ nvim/.local/share/nvim/site/pack/treesitter/start/sections.nvim/queries/python/sections.scm
@@ -54,57 +54,49 @@
 (
   (class_definition
     body: (block
-            (expression_statement
               (assignment
                 left: (identifier) @section.name
                 type: (type (identifier) @section.type_annotation)?
-                )
-              ) @section
-            )
-  )
+                ) @section
+              )
+    )
   (#not-match? @section.name "^_")
   (#set! type "attribute")
-)
+  )
 
 (
-  (class_definition
-    body: (block
-            (expression_statement
-              (assignment
-                left: (identifier) @section.name
-                type: (type (identifier) @section.type_annotation)?
-                )
-              ) @section
-            )
-  )
-  (#match? @section.name "^_")
-  (#set! private "true")
-  (#set! type "attribute")
-)
+ (class_definition
+   body: (block
+           (assignment
+             left: (identifier) @section.name
+             type: (type (identifier) @section.type_annotation)?
+             ) @section
+           )
+   )
+ (#match? @section.name "^_")
+ (#set! private "true")
+ (#set! type "attribute")
+ )
 
 (
-  (module
-    (expression_statement
-      (assignment
-        left: (identifier) @section.name
-        type: (type (identifier) @section.type_annotation)?
-        )
-      ) @section
-  )
-  (#not-match? @section.name "^_")
-  (#set! type "attribute")
-)
+ (module
+   (assignment
+     left: (identifier) @section.name
+     type: (type (identifier) @section.type_annotation)?
+     ) @section
+   )
+ (#not-match? @section.name "^_")
+ (#set! type "attribute")
+ )
 
 (
-  (module
-    (expression_statement
-      (assignment
-        left: (identifier) @section.name
-        type: (type (identifier) @section.type_annotation)?
-        )
-      ) @section
-  )
-  (#match? @section.name "^_")
-  (#set! private "true")
-  (#set! type "attribute")
-)
+ (module
+   (assignment
+     left: (identifier) @section.name
+     type: (type (identifier) @section.type_annotation)?
+     ) @section
+   )
+ (#match? @section.name "^_")
+ (#set! private "true")
+ (#set! type "attribute")
+ )

5cfdea7390cc34e5d8d2b579e4f262c89bf7a9e8
theogilbert <theovgilbert@gmail.com>
2026-05-11 08:11:55 +0200

    dislpay dap breakpoint on stopped lines

--- nvim/.config/nvim/lua/dap-plugins.lua
+++ nvim/.config/nvim/lua/dap-plugins.lua
@@ -11,6 +11,7 @@ end
 
 vim.fn.sign_define('DapBreakpoint', {text='🛑', texthl='', linehl='', numhl=''})
 vim.fn.sign_define('DapBreakpointCondition', {text='🟡', texthl='', linehl='', numhl=''})
+vim.fn.sign_define('DapStopped', {text='', texthl='', linehl='debugPC', numhl=''})
 
 dap.adapters.python = function(cb, config)
   if config.request == 'attach' then
